ec74bb753492637db586f08fb8c9ad7f4e384226
Malek Fhima <malekfhima1@gmail.com>
2025-02-09 11:01:56 +0100

    of

--- insert prod/main.php
+++ insert prod/main.php
@@ -1,6 +1,12 @@
 <?php
-include('../conx.php');
+include('../cnx.php');
 extract($_POST);
+$cat='';
+if(isset($_POST['cat']) && !empty($_POST['cat'])){
+    $cat=$_POST['cat'];
+}elseif(isset($_POST['cat2']) && !empty($_POST['cat2'])){
+    $cat=$_POST['cat2'];
+}
+$res=mysqli_query($cnx, "INSERT INTO prod (image,titel,price,cat,dis) VALUES('$file','$titel','$cat','$dis')");
 
-$res=mysqli_query($cnx, "INSERT INTO prod (image,titel,price,cat,dis) VALUES('$file','$titel','$cat','$)");
 ?>
