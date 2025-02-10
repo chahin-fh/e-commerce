fc3cb314282a2fd05af7a8b86265c16291624786
Malek Fhima <malekfhima1@gmail.com>
2025-02-10 14:57:19 +0100

    signup kmlt c bon

--- sign up/main.php
+++ sign up/main.php
@@ -1,6 +1,13 @@
 <?php
 include("../cnx.php");
 extract($_POST);
-$res = mysqli_query($cnx,"INSERT into client(username,password,adresse,email) values('$username','$password','$adresse','$email')");
+$res1 = mysqli_query($cnx, "SELECT * FROM client WHERE username='$username' or password='$password' or email='$email'");
+$nb=mysqli_num_rows($res1);
+if ($nb>0) {
+    echo "<script>alert('username est deja existe !!');</script>";
+    header('index.html');
+}else{
+    $res = mysqli_query($cnx,"INSERT into client(username,password,adresse,email) values('$username','$password','$adresse','$email')");
+}
 mysqli_close($cnx);
 ?>
