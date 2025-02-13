9b288665e97872650b933ed0fc0ae4697004f29e
Fhima C. <chahinfhima@gmail.com>
2025-02-13 20:10:27 +0100

    tasli7 8altat

---
+++ login/index.php
@@ -0,0 +1,43 @@
+<?php
+include("../cnx.php");
+session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<!-- Coding By CodingNepal - www.codingnepalweb.com -->
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Glassmorphism Login Form | CodingNepal</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="wrapper">
+    <form action="#">
+      <h2>Login</h2>
+        <div class="input-field">
+        <input type="text" required   name="email">
+        <label>Enter your email</label>
+      </div>
+      <div class="input-field">
+        <input type="password" required name="pass">
+        <label>Enter your password</label >
+      </div>
+      <div class="forget">
+        <label for="remember">
+          <input type="checkbox" id="remember">
+          <p>Remember me</p>
+        </label>
+        <a href="#">Forgot password?</a>
+      </div>
+      <button type="submit">Log In</button>
+      <div class="register">
+        <p>Don't have an account? <a href="#">Register</a></p>
+      </div>
+    </form>
+  </div>
+</body>
+</html>
